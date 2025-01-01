64eb7471e690864501e3537ee5b596a8ccd2836f
nihil0000 <nihil.dev0@gmail.com>
2025-01-01 23:25:20 +0900

    feat: implement request validation for Todo creation and enhance index view for success and error messages

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,9 +6,21 @@
 
 @section('content')
 <section class="todo__alert">
+    @if (session('message'))
     <div class="todo__alert--success">
-        Todoを作成しました
+        {{ session('message') }}
     </div>
+    @endif
+
+    @if ($errors->any())
+    <div class="todo__alert--danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li> {{ $error }} </li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 </section>
 
 <section class="todo__content">
